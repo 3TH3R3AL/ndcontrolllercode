c73c329b3bd21d5b864d54e5e16bc7cb534b0107
APResearchAnonymousAccount <thisisforresearchiswear@gmail.com>
2023-06-17 16:29:49 -0400

    started tcp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from controllers import Caen, MHV4
 import json
 import time
+import sys
+import socket
+import select
 
 with open('config.json','r') as f:
     config = json.loads(f.read())
@@ -19,13 +22,52 @@ devices = {
     "MHV4": mhv4
 }
 
-while True:
-    command = json.loads(input("Command:"))
-    device = devices[command["device"]]
-    if(command["action"] == "set_on"):
-        device.set_on(command["channel"])
-    elif(command["action"] == "set_off"):
-        device.set_on(command["channel"])
-    elif(command["action"] == "heartbeat"):
-        print(device.heartbeat())
-    time.sleep(1)
+
+TCP_IP = sys.argv[1]
+TCP_PORT = 8888
+BUFFER_SIZE = 1024
+param = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((TCP_IP,TCP_PORT))
+server.listen(1)
+rxset = [server]
+txset = []
+
+while 1:
+    rxfds, txfds, exfds = select.select(rxset, txset, rxset)
+
+    for sock in rxfds:
+        if sock is server:
+            conn, addr = server.accept()
+            conn.setblocking(0)
+            rxset.append(conn)
+            print('Connection from address:', addr)
+        else:
+            try:
+                data = sock.recv(BUFFER_SIZE)
+                if data == ";" :
+                    print("Received all the data")
+                    for x in param:
+                        print(x)
+                    param = []
+                    rxset.remove(sock)
+                    sock.close()
+                else:
+                    command = json.loads(data)
+                    device = devices[command["device"]]
+                    if(command["action"] == "set_on"):
+                        device.set_on(command["channel"])
+                    elif(command["action"] == "set_off"):
+                        device.set_on(command["channel"])
+                    elif(command["action"] == "heartbeat"):
+                        print(device.heartbeat())
+                    param.append(data)
+            except:
+                print("Connection closed by remote end")
+                param = []
+                rxset.remove(sock)
+                sock.close()
+
+
+    
