af01706f370cfe0607136750d152219e65b86d94
APResearchAnonymousAccount <thisisforresearchiswear@gmail.com>
2023-06-27 16:33:25 -0400

    m

--- simplesever.py
+++ simplesever.py
@@ -2,10 +2,10 @@ import socket
 import urllib.parse
 def handle_client(client_socket):
     while True:
-        data = client_socket.recv(1024).decode('utf-8')
+        data = client_socket.recv(1024)
         if not data:
             break
-        print(urllib.parse.quote(data), end='')
+        print(data, end='')
 
     client_socket.close()
 
