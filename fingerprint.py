0d297b6456b4ddd4e01c047e7381a33b3bccc698
APResearchAnonymousAccount <thisisforresearchiswear@gmail.com>
2023-06-27 15:55:38 -0400

    d

--- fingerprint.py
+++ fingerprint.py
@@ -46,14 +46,14 @@ def fingerprint(port):
         ser.flushOutput()
         ser.flushInput()
         ser.close()
-        '''
-        mhv4 = MHV4(port,9600,[ 0.0, 6553.5, 50.0, 40.0],2.5)
+        
+        mhv4 = MHV4(port,9600,[ 0.0, 0, 0.0, 0.0],2.5)
         for device in config['devices']:
             if(config['devices'][device]['controller'] == "MHV4"):
                 config['devices'][device]['port'] = port
                 config['devices'][device]['voltages'] = [mhv4.get_voltage_preset(i) for i in range(1,4)]
         mhv4.close()
-        '''
+        
     else:
         print(response)
         ret['type'] = "Unknown"
