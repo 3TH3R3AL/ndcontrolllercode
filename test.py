73141d6b61f046dcc80fec9d051879d041bb8b76
APResearchAnonymousAccount <thisisforresearchiswear@gmail.com>
2023-07-07 15:02:18 -0400

    d

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
 import threading
 import time
-test = ['test',"test2"]
 # Define a function that will be executed in a separate thread
-def thread_function():
+def thread_function(test):
     # Code to be executed in the thread
-    time.sleep(2)
     print("This is running in a separate thread",test)
     
     test.pop()
 
 # Create a thread object
-thread = threading.Thread(target=thread_function)
+thread = threading.Thread(target=thread_function(1))
 # Start the thread
 print('test')
 thread.start()
 
 # Code to be executed in the main thread
-print("This is running in the main thread",test)
+print("This is running in the main thread",1)
 
 # Wait for the thread to finish
 thread.join()
 
 # Code after the thread has finished
-print("Thread has finished",test)
+print("Thread has finished",2)
