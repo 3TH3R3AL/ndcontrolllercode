1dda5d22754543040f6a9f4638bca7bd2963f884
APResearchAnonymousAccount <thisisforresearchiswear@gmail.com>
2023-07-07 16:40:20 -0400

    d

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ while nbreak:
                             if(device != {}): device.close()
                         break
                     elif(command["action"] == "set_on" or command["action"] == "set_off"):
+                        print(command["action"],"added to queue")
                         device.queue.appendleft(command)
                     else:
                         device.queue.append(command)
