41980a87a98712a70bcbcc96e29232b7a8fd2a05
APResearchAnonymousAccount <thisisforresearchiswear@gmail.com>
2023-07-11 16:07:40 -0400

    d

--- controllers.py
+++ controllers.py
@@ -100,7 +100,10 @@ class Caen:
         self.sock = sock
         while self.processing:
             if self.queue:
-                processCommand(self.queue.popleft(),self)
+                try:
+                    processCommand(self.queue.popleft(),self)
+                except Exception as e:
+                    print(e)
             else:
                 time.sleep(0.1)
 
